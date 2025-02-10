daa091410cfd3c2d3cd2af5e373a2d71b9535d73
minakaragoz <160979538+minakaragoz@users.noreply.github.com>
2025-02-09 23:05:13 -0500

    Add files via upload

---
+++ lab04/lab04.py
@@ -0,0 +1,33 @@
+from pathlib import Path
+import sys
+
+sys.path.append(str(Path(__file__).parents[1]))
+
+from util.llm_utils import TemplateChat
+
+def run_console_chat(sign, **kwargs):
+    chat = TemplateChat.from_file(sign=sign, **kwargs)
+    chat_generator = chat.start_chat()
+    print(next(chat_generator))
+    while True:
+        try:
+            message = chat_generator.send(input('You: '))
+            print('Agent:', message)
+        except StopIteration as e:
+            if isinstance(e.value, tuple):
+                print('Agent:', e.value[0])
+                ending_match = e.value[1]
+                print('Ending match:', ending_match)
+            break
+
+lab04_params = {
+        "template_file": "lab04_trader_chat.json", 
+        "sign": "Mina",  
+        "end_regex": r"trade complete", 
+    }
+
+
+if __name__ ==  '__main__':
+    # run lab04.py to test your template interactively
+
+    run_console_chat(**lab04_params)
